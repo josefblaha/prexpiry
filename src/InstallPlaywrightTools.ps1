c44f70ac94287685509b68932b3817bc5b7b3e22
Josef Bláha <josef.blaha@blocshop.io>
2024-01-07 17:26:31 +0100

    #1 Blazor App from template + first E2E test

---
+++ src/InstallPlaywrightTools.ps1
@@ -0,0 +1,1 @@
+﻿& Prexpiry.Tests/bin/Debug/net8.0/playwright.ps1 install
---
+++ src/Prexpiry.Tests/IntegrationTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright;
+using static Microsoft.Playwright.Assertions;
+
+namespace Prexpiry.Tests;
+
+public class IntegrationTests
+{
+    [Fact]
+    public async Task Walking_skeleton_with_empty_table()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("https://prexpiry-e2e.azurewebsites.net");
+        
+        await Expect(page.GetByRole(AriaRole.Heading, new PageGetByRoleOptions { Name = "Secrets" }))
+            .ToBeVisibleAsync();
+
+        await Expect(page.GetByRole(AriaRole.Table).Locator("tr")).ToHaveCountAsync(1);
+    }
+}
